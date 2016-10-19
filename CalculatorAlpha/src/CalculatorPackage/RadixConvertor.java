99a40cd9d58c2f050213a7414bde6a58bcadf249
su <suchen_xi@163.com>
2016-10-20 00:20:40 +0800

    1，将进制转换合并 2，两个变量运算增至多个

--- CalculatorAlpha/src/CalculatorPackage/RadixConvertor.java
+++ CalculatorAlpha/src/CalculatorPackage/RadixConvertor.java
@@ -9,7 +9,7 @@ import java.util.Scanner;
 
 /**
  *
- * @author TESLA_CN
+ * @author susu_xi
  */
 public class RadixConvertor {
     
@@ -21,55 +21,66 @@ public class RadixConvertor {
        this.radix = radix;
     }
     
-    public String OthersToDecimal(String expression, Integer radix) {
-        String decimal = "";
-        switch(radix){
-            //其他进制转十进制
-            case 2: decimal = Integer.valueOf(expression,2).toString(); break;
-            case 8: decimal = Integer.valueOf(expression,8).toString(); break;
-            case 16:decimal = Integer.valueOf(expression,16).toString();break;
-        }
-        return decimal;   
-    }
-    
-    public String DecimalToOthers(int decimal, Integer radix){
-        //十进制转其他进制
+ 
+    public String RandomConvert(String source, Integer radix, Integer targetradix){
         String convertresult = "";
         switch(radix){
-            case 2: convertresult = Integer.toBinaryString(decimal);break;
-            case 8: convertresult = Integer.toOctalString(decimal); break;
-            case 16:convertresult = Integer.toHexString(decimal);   break;
+            case 2: if(targetradix == 8) 
+                        convertresult = Integer.toOctalString(Integer.parseInt(source, 2));
+                    else if(targetradix == 10)
+                        convertresult = Integer.valueOf(source,2).toString();
+                    else if(targetradix == 16)
+                        convertresult = Integer.toHexString(Integer.parseInt(source,2));
+                    break;
+            case 8: if(targetradix == 2) 
+                        convertresult = Integer.toBinaryString(Integer.parseInt(source, 8));
+                    else if(targetradix == 10)
+                        convertresult = Integer.valueOf(source,8).toString();
+                    else if(targetradix == 16)
+                        convertresult = Integer.toHexString(Integer.parseInt(source, 8));
+                    break;
+            case 10:if(targetradix == 2) 
+                        convertresult = Integer.toBinaryString(Integer.parseInt(source, 10));
+                    else if(targetradix == 8)
+                        convertresult = Integer.toOctalString(Integer.parseInt(source, 10));
+                    else if(targetradix == 16)
+                        convertresult = Integer.toHexString(Integer.parseInt(source, 10));
+                    break;
+            case 16:if(targetradix == 2) 
+                        convertresult = Integer.toBinaryString(Integer.parseInt(source, 16));
+                    else if(targetradix == 8)
+                        convertresult = Integer.toOctalString(Integer.parseInt(source, 16));
+                    else if(targetradix == 10)
+                        convertresult = Integer.valueOf(source,16).toString();
+                    break;
         }
         return convertresult;
     }
-    
-    public String getDecimalExpression(String expression, Integer radix) {
+
+    public String getRandomExpression(String expression, Integer radix, Integer targetradix) {
+        
         Scanner in = new Scanner(expression);
-        String number1 = in.next();  //第一个运算数
-        String operator = in.next(); //运算符
-        String number2 = in.next();  //第二个运算数
-        number1 = OthersToDecimal(number1,radix);
-        number2 = OthersToDecimal(number2,radix);
-        expression = number1+" "+operator+" "+number2;
-        return expression;
+        StringBuffer stringbuffer = new StringBuffer();
+        while (in.hasNext()) {
+            String temp = in.next();
+            if(temp.matches("[+-/*()]")){
+                stringbuffer.append(temp);
+                stringbuffer.append(" ");
+            }else{
+                temp = RandomConvert(temp, radix, targetradix);
+                stringbuffer.append(temp);
+                stringbuffer.append(" ");
+            }
+        }
+        return stringbuffer.toString();
     }
     
-    public String DecimalToOthersExpression(String expression,Integer radix) {
-        //获取二进制，八进制或十六进制形式的表达式
-        //radix 为目标转换数制
-        Scanner in = new Scanner(expression);
-        int number1 = in.nextInt();  //第一个运算数
-        String operator = in.next(); //运算符
-        int number2 = in.nextInt();  //第二个运算数
-        String number1Binary = DecimalToOthers(number1,radix);
-        String number2Binary = DecimalToOthers(number2,radix);
-        expression = number1Binary+" "+operator+" "+number2Binary;
-        return expression;
-    }
+    
         public static void main(String args[]){
-        String expression = "122 + 65535";
-        RadixConvertor a = new RadixConvertor(expression,10);
-        expression = a.DecimalToOthersExpression(expression,2);
+        String expression = "( 111 + 111010 ) * 110101 + 100000";
+        RadixConvertor a = new RadixConvertor(expression,2);
+        expression = a.getRandomExpression(expression, 2 ,10);
+       // expression = a.RandomConvert(expression,2);
         System.out.println(expression);
     }
 
