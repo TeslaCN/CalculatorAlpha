e1101ff412203dc056f02911722f1a6f872cef1c
Cjy-CN <scau.cjy@gmail.com>
2016-10-20 19:07:17 +0800

    表达式解析的注释

--- CalculatorAlpha/src/CalculatorPackage/Algorithm.java
+++ CalculatorAlpha/src/CalculatorPackage/Algorithm.java
@@ -15,15 +15,16 @@ import java.util.*;
  * 
  */
 public class Algorithm {
+//用一个数字栈和一个符号栈来存储解析式(见大一数据结构)
+    private final Stack<Double> number = new Stack<>();//数字栈
+    private final Stack<String> symbol = new Stack<>();//符号栈
+    private static final Map<String, Integer> cmp = new HashMap<>();//map用来存储符号以及对于的运算优先级,不用原来的数组存储方法,算是一点改进吧
 
-    private final Stack<Double> number = new Stack<>();
-    private final Stack<String> symbol = new Stack<>();
-    private static final Map<String, Integer> cmp = new HashMap<>();
-
-    private String expr;
+    private String expr;//表达式
 /**
- * 
+ * map每个元素包含两个值,第一个是符号,第二个是符号对应的运算优先级,数字越大,优先级越高
  * @param String 表达式 
+ * 
  */
     public Algorithm(String expression) {
         expr = expression;
@@ -46,6 +47,7 @@ public class Algorithm {
         Scanner expression = new Scanner(expr);
         String temp = expression.next();
         while (!("#".equals(temp)) || !("#".equals(symbol.peek()))) {
+            //正则表达式判断数字和几个应该被压入数字栈的特殊符号
             if (temp.matches("[+-]?\\d.*") || temp.matches("[+-]?π")||temp.matches("[+-]?Ans")||temp.matches("[+-]?e")) {
                 if (temp.matches("[+-]?π")) {
                     if (temp.charAt(0)=='-') {
@@ -67,6 +69,7 @@ public class Algorithm {
                     number.push(temp_num);
                 }
                 temp = expression.next();
+                //判断基本符号,并通过map获取运算优先级判断该把符号入栈还是弹栈进行运算
             } else if (temp.equals("#")||temp.equals("+")||temp.equals("-")||
                     temp.equals("*")||temp.equals("/")||temp.equals("(")||temp.equals(")")) {
                 if (cmp.get(symbol.peek()) < cmp.get(temp)) {
@@ -80,11 +83,12 @@ public class Algorithm {
                     Double a = number.pop();
                     String suanzi = symbol.pop();
                     number.push(calculate(a, suanzi, b));
-                    //System.out.println(i+"此时栈顶优先级高,弹出栈");
                 } else if ("(".equals(symbol.peek())) {
                     symbol.push(temp);
                     temp = expression.next();
                 }
+                //else包含的是遇到sin,cos,log...这些特殊符号的操作
+                //通过括号匹配原则,把sin(...) cos(...)这些交给另外一个类处理
             } else {
                 StringBuilder subExpression = new StringBuilder();
                 subExpression.append(temp).append(" ");
@@ -105,7 +109,7 @@ public class Algorithm {
         }
         return number.peek();
     }
-
+//进行简单的四则运算操作
     private Double calculate(Double a, String s, Double b) {
         switch (s) {
             case "+":
