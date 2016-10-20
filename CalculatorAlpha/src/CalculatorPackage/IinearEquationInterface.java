aea3c34e5b120886351ca7e5e0cad44fa31cdde1
TeslaCN <root496879118@me.com>
2016-10-20 10:44:37 +0800

    🌚解方程界面类

---
+++ CalculatorAlpha/src/CalculatorPackage/IinearEquationInterface.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package CalculatorPackage;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author TESLA_CN
+ */
+public class IinearEquationInterface extends Application {
+    
+    @Override
+    public void start(Stage primaryStage) {
+
+        
+        primaryStage.show();
+    }
+    
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+}
